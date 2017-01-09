d3e70ce3edaf2c06b555cd5a3e70f890a26ebcde
Student6 <Student6@10.8.33.148>
2017-01-09 13:53:42 -0500

    push

--- SimonRichard/src/simon/ProgressWendy.java
+++ SimonRichard/src/simon/ProgressWendy.java
@@ -1,41 +1,59 @@
 package simon;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
 
 import guiPratice.components.Component;
 
 public class ProgressWendy extends Component implements ProgressInterfaceRichard {
 	
-	private int round;
+	private String round;
 	private int size;
+	private boolean gameOver;
+	private int x;
+	private int y;
+	private int w;
+	private int h;
+	
 
 	public ProgressWendy(int x, int y, int w, int h) {
 		super(x, y, w, h);
 		// TODO Auto-generated constructor stub
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
 	}
 
 	@Override
 	public void gameOver() {
 		// TODO Auto-generated method stub
-
+		gameOver = true;
+		update();
 	}
 
 	@Override
 	public void setRound(int x) {
 		// TODO Auto-generated method stub
-			round = x;
+		round = "Round " + x;
+		update();
 	}
 
 	@Override
 	public void setSequenceSize(int x) {
 		// TODO Auto-generated method stub
-			size = x;
+		size = x;
+		update();
 	}
 
 	@Override
-	public void update(Graphics2D arg0) {
+	public void update(Graphics2D g) {
 		// TODO Auto-generated method stub
-
+		if(gameOver)
+		{
+			g.setColor(Color.BLACK);
+			g.drawString("Game Over", x +10, y+10);
+		}
 	}
 
 }
